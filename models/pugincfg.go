c13b5b42eaa26be7a40980f4b094a2608eb48fbc
yeqown <yeqown@gmail.com>
2019-03-06 15:25:44 +0800

    u

--- models/pugincfg.go
+++
@@ -1,8 +0,0 @@
-package models
-
-// NocacheCfg structs for plugin.proxy
-type NocacheCfg struct {
-	Idx     string `json:"idx"`
-	Regexp  string `json:"regexp"`
-	Enabled bool   `json:"enabled"`
-}
